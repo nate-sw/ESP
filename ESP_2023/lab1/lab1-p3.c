aa45da3b41f852879ab2acb6f421adfc797b7315
nate-sw <nathanaelsimardwhite@gmail.com>
2023-01-23 12:08:08 -0500

    Lab 1 Update 2

---
+++ ESP_2023/lab1/lab1-p3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+Program: Lab 1 Part 3
+Author: nsw
+Date: 2023/01/23
+Github Location: https://github.com/nate-sw/ESP/ESP_2023/lab1
+MOTD: Most fish are ectothermic, allowing their body temperatures to vary as ambient temperatures change.
+
+
+*/
+
+int main(){
+
+    printf("\nBy:\tnsw\n");
+    printf("\nHello,\tWorld!\n");
+    return 0;
+
+}
---
+++ ESP_2023/lab1/lab1-p4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+Program: Lab 1 Part 4
+Author: nsw
+Date: 2023/01/23
+Github Location: https://github.com/nate-sw/ESP/ESP_2023/lab1
+MOTD: Most fish are ectothermic, allowing their body temperatures to vary as ambient temperatures change.
+
+
+*/
+
+int main(){
+
+    printf("\x0A\x0D\x42\x79\x3A\x09\x6E\x73\x77\x0A\x0D");
+    printf("\x0A\x0D\x48\x65\x6C\x6C\x6F\x2C\x09\x57\x6F\x72\x6C\x64\x21\x0A\x0D");
+    return 0;
+
+}
